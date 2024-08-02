c9eacda11593b8fda8b246646bba5832a92a93b5
Joao Paulo <joaopauloj405@gmail.com>
2024-08-02 07:59:06 -0300

    feat: add validation & custom logging

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,9 +1,20 @@
+import { Type } from '@nestjs/class-transformer';
+import { IsDate, IsNotEmpty, IsString } from '@nestjs/class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
 
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
 
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
 
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
